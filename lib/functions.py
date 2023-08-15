7808564c71ce8181b32022bfbb4204ae2047d90a
Kelvin Kapaya <kalvinkapu@gmail.com>
2023-08-15 10:23:09 +0300

    Functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print("Hello, Guido!")
+    
+# greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+    # greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+    print(add)
+    add(45, 55)
 
 def halve(number):
-    pass
+    return number/2
+    
+
